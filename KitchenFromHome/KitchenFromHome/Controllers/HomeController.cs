aa5938b853ed71016b53a5483898f21d7a496681
藤田健 <>
2020-04-29 00:07:06 +0900

    トップページ完成

--- KitchenFromHome/KitchenFromHome/Controllers/HomeController.cs
+++ KitchenFromHome/KitchenFromHome/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace KitchenFromHome.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Cover()
         {
             return View();
         }
